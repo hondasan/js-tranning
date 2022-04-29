39d90d31959585e06ead7492dde4cbd9c74f4289
hondasan <86158971+hondasan@users.noreply.github.com>
2022-04-29 23:33:50 +0900

    constについて

--- src/index.js
+++ src/index.js
@@ -21,3 +21,25 @@
 
 // // letは再宣言不可能
 // let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// const変数は上書き不可
+//val3 = "const変数を上書き";
+//const val3 = "const変数を再宣言";
+
+// const で定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "taka",
+//   age: 28,
+// }
+// val4.name = "honda";
+// val4.address = "sap"
+// console.log(val4);
+
+// const で定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
