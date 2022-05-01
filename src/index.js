2765b1c05e3c126164220d9d2f35fafa027a6eb8
hondasan <86158971+hondasan@users.noreply.github.com>
2022-05-01 23:19:07 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -187,3 +187,20 @@
 //   }
 // });
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrue : 条件がfalse
+// const val1 = 1 > 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = "1300";
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲ないです";
+// };
+// console.log(checkSum(50, 50));
